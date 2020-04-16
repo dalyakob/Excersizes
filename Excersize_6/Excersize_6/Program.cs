da30c0fade362affe1466fddf6756b174c064568
dalyakob <dalyakob@umich.edu>
2020-04-15 22:34:56 -0400

    Excersize 6 and excersize 7

---
+++ Excersize_6/Excersize_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Excersize_6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var condition = "n";
+
+            do
+            {
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("Would you like to continue (y/n)?");
+                condition = Console.ReadLine();
+            } while (condition == "y");
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ Excersize_7/Excersize_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Excersize_7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String userInput, condition = "n";
+
+            do
+            {
+                Console.WriteLine("Enter some text: ");
+                userInput = Console.ReadLine();
+
+                Console.WriteLine("Would you like to continue?");
+                condition = Console.ReadLine();
+            } while (condition == "y");
+            Console.WriteLine("Goodbye!");
+        }
+
+    }
+}
