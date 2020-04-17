5fe52f8e48680a3bf0516302d1e632c4ea985ae9
dalyakob <dalyakob@umich.edu>
2020-04-16 20:58:13 -0400

    Completed Exercise 13 :)

---
+++ Exercise_13/Exercise_13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_13
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string condition;
+            do
+            {
+                Console.WriteLine("Enter a number: ");
+                string userInput = Console.ReadLine();
+                for (int num = int.Parse(userInput); num >= 0; num--)
+                {
+                    Console.WriteLine(num);
+                }
+                Console.WriteLine("Would you like to continue? (y/n)");
+                condition = Console.ReadLine().ToLower();
+            } while (condition == "y");
+        }
+    }
+}
