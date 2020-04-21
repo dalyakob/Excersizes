5db32dcc7db1fa21f122d258218818ad6d4fbb98
dalyakob <dalyakob@umich.edu>
2020-04-20 21:20:20 -0400

    Exercise 16 completed

---
+++ Exercise_16/Exercise_16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_16
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int height;
+            bool valid;
+            string userInput;
+            do
+            {
+                do
+                {
+                    Console.Write("Enter a number: ");
+                    valid = int.TryParse(Console.ReadLine(), out height);
+                } while (!valid);
+                string str = "*";
+                for (int i = 0; i < height; i++)
+                {
+                    Console.WriteLine(str);
+                    str += " *";
+                }
+                Console.WriteLine("Would you like to continue(y/n)? ");
+                userInput = Console.ReadLine().ToUpper();
+            } while (userInput == "Y");
+        }
+    }
+}
